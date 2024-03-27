73e0deb58194d3009f43669030debbb5ab0a840c
ThangDevAlone <quangthangvtlg@gmail.com>
2024-03-27 09:08:03 +0700

    hee

--- Account/permissions.py
+++ Account/permissions.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission,SAFE_METHODS
 
 class IsUser(BasePermission):
     """
@@ -7,3 +7,8 @@ class IsUser(BasePermission):
 
     def has_permission(self, request, view):
         return request.user and request.user.is_staff
+class IsOwnerOrReadonly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.author == request.user
